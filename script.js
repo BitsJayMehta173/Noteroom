d4da0bfc2d215d03df3573594ca0d518c68c499f
Jay Mehta <78312356+BitsJayMehta173@users.noreply.github.com>
2024-05-30 18:45:03 +0530

    added add button functionality for new file

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 wordcount=document.getElementsByClassName("wordcount")
+addbtn=document.getElementById("addbtn")
+btncover=document.querySelector(".openedfile")
+
 
 ta=document.querySelector(".ta")
 
+function addLab(){
+    let btndiv=document.createElement("div")
+    btndiv.classList.toggle("btncover")
+    let name=document.createElement("button")
+    let close=document.createElement("button")
+    name.innerHTML="Life"
+    close.innerHTML="X"
+    close.classList.toggle("close")
+    btndiv.appendChild(name)
+    btndiv.appendChild(close)
+    btncover.appendChild(btndiv)
+}
 
 ta.addEventListener("keyup", function(){
     let con=ta.value
