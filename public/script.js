b826e044fbe1b7ebbec1b49b31e3b73bfd856dc6
Jay Mehta <78312356+BitsJayMehta173@users.noreply.github.com>
2024-06-05 01:55:52 +0530

    added a story for upload
    Just a Bday special

--- public/script.js
+++ public/script.js
@@ -10,6 +10,58 @@ close=document.querySelectorAll(".close")
 lastactivefile=null
 ta=document.querySelector(".ta")
 
+ta.value=".....Sunflower....
+
+a small hand pasees,
+running in those crowds of sunflower,
+giggling with laughter,
+and stops by a beautiful sunflower,
+touches it with silence,
+mommy do you know why dad loves it soo much?
+why dont you ask him yourself girl..
+im sure dad will love it.
+you know sunflower always waits for the sun..
+now lets go and show it to your dad.
+plucks the flower
+and runs to the cottage happily..
+
+nailing sound comes from a room,
+someone is packing a box.
+bhoo and giggles.
+dady i got you haha.
+yes that scared me dear hahaha,
+hehehe thats what a villian power is hehehe (evil laugh)
+hahaha(worried dad laugh)
+laughs daughter.
+
+tadaaa see what i brought..
+(smiles) yes thats what we needed now thanks cookie,
+daddy did you put my letter in it.
+yes i did dear.
+so now your mama must be waiting
+sooo its all ready now lets go.
+let me carry the box dady.
+uuuggghh its heavy dad.
+hahaha come let me get it, you take the flowers..
+okk dady(sad)
+
+(carries the wooden box)leaves the room
+
+as they walk..
+daddy why do you like sunflower soo much..
+smiles because your mom always gave me sunflower when we used to meet.
+but today we are going to give it to her.
+
+we are here she runs foreward,
+(there lies a gravestone,)
+dad follows..
+a silent wind blows as we se the gravestone.
+dad steps foreward with a silent expression 
+and sits in front of the gravestone.
+come sit here.
+and give it to your mom.....
+places the sunfllower over the gravestone...."
+
 function highlight(){
 
     // seperated into two function taking a lot of time and lines of code merge it or find a alternative solution
